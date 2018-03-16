795b7d0abf18d4ad94fe684a0935529bda973a90
Darren <psotails@gmail.com>
2018-03-16 03:32:04 -0500

    Finally Done Thank GOD....

--- Program_Test/database.h
+++
@@ -1,6 +0,0 @@
-#include <sqlite3.h>
-
-void setupDB(int bank);
-int accountLookUp(char* accountNum);
-int credit(int bank, char*accountNum, int amount);
-int debit(int bank, char* accountNum, int amount);
